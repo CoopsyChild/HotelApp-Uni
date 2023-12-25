7d2be50a61fd4865a2b7124f250faef2f2401d57
kupszi <f.farkas.erik998@gmail.com>
2023-12-25 14:56:06 +0100

    Reservation class extended with HotelId

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -2,20 +2,32 @@ namespace HotelApp.Models;
 
 public class Reservation
 {
+    public Reservation(string userId, DateTime startTime, DateTime endTime, string id, string hotelId, RoomId roomId)
+    {
+        UserId = userId;
+        StartTime = startTime;
+        EndTime = endTime;
+        Id = id;
+        HotelId = hotelId;
+        RoomId = roomId;
+    }
+
+    public Reservation(string userId, DateTime startTime, DateTime endTime, string hotelId, RoomId roomId)
+    {
+        UserId = userId;
+        StartTime = startTime;
+        EndTime = endTime;
+        HotelId = hotelId;
+        RoomId = roomId;
+    }
+
+    public String? Id { get; }
+    public String HotelId { get; }
     public RoomId RoomId { get; }
     public String UserId;
     public DateTime StartTime;
     public DateTime EndTime;
     public TimeSpan Length => EndTime.Subtract(StartTime);
-
-    public Reservation(DateTime startTime, DateTime endTime, RoomId roomId, String userId)
-    {
-        StartTime = startTime;
-        EndTime = endTime;
-        RoomId = roomId;
-        UserId = userId;
-    }
-
     public bool Conflicts(Reservation reservation)
     {
         if (!reservation.RoomId.Equals(RoomId))
