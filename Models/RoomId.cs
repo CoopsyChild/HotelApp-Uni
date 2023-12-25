ee04fde9e96105255a10e0e8ec0c25991239c008
kupszi <f.farkas.erik998@gmail.com>
2023-12-25 14:54:34 +0100

    extended RoomId to use HotelId for id generation

--- Models/RoomId.cs
+++ Models/RoomId.cs
@@ -2,28 +2,31 @@ namespace HotelApp.Models;
 
 public class RoomId
 {
-    public RoomId(int floorNumber, int roomNumber)
+    public RoomId(String hotelId, int floorNumber, int roomNumber)
     {
         FloorNumber = floorNumber;
         RoomNumber = roomNumber;
+        HotelId = hotelId;
     }
     public int FloorNumber { get; }
     public int RoomNumber { get; }
+    public String HotelId { get; }
 
     public override bool Equals(object? obj)
     {
         return obj is RoomId roomId &&
                roomId.FloorNumber == FloorNumber &&
-               roomId.RoomNumber == RoomNumber;
+               roomId.RoomNumber == RoomNumber &&
+               roomId.HotelId == HotelId;
     }
 
     public override string ToString()
     {
-        return $"{FloorNumber}{RoomNumber}";
+        return $"{HotelId}{FloorNumber}{RoomNumber}";
     }
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(FloorNumber, RoomNumber);
+        return HashCode.Combine(HotelId, FloorNumber, RoomNumber);
     }
 }
